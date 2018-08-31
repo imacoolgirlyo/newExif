be124a1d460f1f451714d13325a7921e23543359
imacoolgirlyo <tjrud1438@naver.com>
2018-08-31 14:41:33 +0900

    then return error

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const handleListening = () => {
-    console.log(`✅ Server Running on : http://localhost:8080`);
+    console.log(`✅ ✅ ✅ ✅ ✅ ✅ ✅ ✅ Server Running on : http://localhost:8080 ✅ ✅ ✅ ✅ ✅ ✅ ✅ `);
 }
 
 const moveFile = (file , name) => {
@@ -34,6 +34,7 @@ const moveFile = (file , name) => {
 }
 
 
+
 app.post('/upload', function(req, res){
     if(!req.files)
         return res.status(400).send('No files were uploaded');
@@ -43,10 +44,15 @@ app.post('/upload', function(req, res){
 
     moveFile(uploadedFile, uploadedFileName)
         .then(result => fs.readFile('./src/'+ result))
-        .then(meta => exif.metadata(meta, function(err, metadata){
-            if(err) throw err;
-            else console.log(metadata);
+        .then(meta => exif.metadata(meta, function(err, data){
+           if(err) {
+               res.send(err)
+            }
+           else{
+             return data
+           } 
         }))
+        .then(data => console.log(data))
         .catch(err => console.error(err));
 
         // .then(exifImage => exif.metadata(exifImage))
